96c3476a3f9bfc60729edb4bdea6b1c1eef22425
Zexi Kuang <zeke_github@gmx.com>
2024-09-17 23:28:14 -0700

    complete 2.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import Kanbas from "./Kanbas";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.tsx</code> and save to reload. */}
-          welcome
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
-
-export default App;
+// export default App;
---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,7 @@
+export default function Kanbas() {
+    return (
+        <div id="wd-kanbas">
+            <h1>Kanbas</h1>
+        </div>
+    );
+}
