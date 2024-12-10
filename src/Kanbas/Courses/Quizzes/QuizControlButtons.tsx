807fcdb88974f2bded9ebce2aa3f615d008814bd
Zexi Kuang <zeke_github@gmx.com>
2024-12-10 12:20:54 -0800

    addquizz3dotsmenu

--- src/Kanbas/Courses/Quizzes/QuizControlButtons.tsx
+++ src/Kanbas/Courses/Quizzes/QuizControlButtons.tsx
@@ -5,16 +5,25 @@ import { deleteQuiz } from "./reducer";
 import { useDispatch } from "react-redux";
 import * as quizzesClient from "./client";
 import * as questionsClient from "./Questions/client";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 
-export default function QuizControlButtons({quizID} : {quizID: string}) {
 
+export default function QuizControlButtons({ quizID }: { quizID: string }) {
+
+  const [showMenu, setShowMenu] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   const deleteCurrentQuiz = async () => {
     const isConfirmed = window.confirm("Are you sure you want to delete this quiz?");
     if (isConfirmed) {
       const questions = await quizzesClient.getQuestions(quizID);
-      for (let i=0; i < questions.length; i++) {
+      for (let i = 0; i < questions.length; i++) {
         await questionsClient.deleteQuestion(questions[i]._id);
       }
       await quizzesClient.deleteQuiz(quizID);
@@ -22,11 +31,24 @@ export default function QuizControlButtons({quizID} : {quizID: string}) {
     }
   }
 
+  const editQuiz = () => {
+    navigate(`/Kanbas/Courses/Quizzes/${quizID}/Details`);
+  };
+
   return (
     <div className="float-end">
-      <FaTrash className="text-danger me-2 mb-1" onClick={deleteCurrentQuiz}/>
+      <FaTrash className="text-danger me-2 mb-1" onClick={deleteCurrentQuiz} />
       <GreenCheckmark quizID={quizID} />
-      <IoEllipsisVertical className="fs-4" />
+      <IoEllipsisVertical className="fs-4" onClick={toggleMenu} />
+      {showMenu && (
+        <div className="position-absolute bg-white border rounded shadow p-2" style={{ right: 0 }}>
+          <button className="btn btn-link text-dark" onClick={editQuiz}>Edit</button>
+          <button className="btn btn-link text-dark" onClick={deleteCurrentQuiz}>Delete</button>
+          <GreenCheckmark quizID={quizID} />
+        </div>
+      )}
     </div>
-    );
+  );
 }
+
+
